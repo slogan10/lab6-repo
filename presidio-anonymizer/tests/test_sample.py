f8f0770d263cd507734fb3efd8850f618d851e90
Logan Sekutowski <232259444+slogan10@users.noreply.github.com>
2025-10-22 21:31:51 +0000

    added a test for sample

--- presidio-anonymizer/tests/test_sample.py
+++ presidio-anonymizer/tests/test_sample.py
@@ -3,4 +3,4 @@ from presidio_anonymizer.sample import sample_run_anonymizer
 
 def test_sample_run_anonymizer():
     # replace the following line with your test
-    pass
+    assert sample_run_anonymizer("My name is Bond.", 11,15) == sample_run_anonymizer("My name is Bond.", 11, 15)
